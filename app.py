e7e9b9d1f1e9087feaeb2abd4f766f477f30d0d6
g2snooker99-beep <g2snooker99@gmail.com>
2026-05-05 03:50:35 +0000

    debug: cleaner name print

--- app.py
+++ app.py
@@ -936,7 +936,8 @@ def shop_schedule():
     all_emps = conn.execute("SELECT * FROM employees").fetchall()
 
     import unicodedata as _ud
-    print("DEBUG all_emps:", [(e['name'], e['name'].encode('unicode_escape')) for e in all_emps])
+    print("DEBUG NAMES:", [e['name'] for e in all_emps])
+    print("DEBUG ROT2:", repr(rot2_name), "FIXED:", repr(fixed_name))
     def find_emp(*keywords):
         for e in all_emps:
             name_n = _ud.normalize('NFC', e['name'])
